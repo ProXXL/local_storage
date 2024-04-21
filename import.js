219332d8b865124d8728e33b5c096a5133c44fd6
ProXXL <166551355+ProXXL@users.noreply.github.com>
2024-04-21 21:54:44 +0300

    Add files via upload

--- import.js
+++ import.js
@@ -2,6 +2,5 @@ for(let i = 0; i < localStorage.length; i++ )
 {
     const key = localStorage.key(i);
     let temp = JSON.parse(localStorage.getItem(key));
-    console.log(temp);
     document.getElementById("display_" + (i+1)).value = temp;
 }
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("Send").addEventListener("click",function clg(){
     localStorage.clear;
-    let con_log = document.getElementById("Name").value;
-    localStorage.setItem("aperson",JSON.stringify(con_log));
     let gender = document.getElementById("gender").value;
     localStorage.setItem("gender",JSON.stringify(gender));
-    open("info.html");
+    let con_log = document.getElementById("Name").value;
+    localStorage.setItem("person",JSON.stringify(con_log));
+    open("info.html" , "_self");
 });
 
